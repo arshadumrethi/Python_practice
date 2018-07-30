7b0d927433a2c9a9e896af3e18b1dec5c3c9dfd8
arshadumrethi <arshad.umrethi@gmail.com>
2018-07-30 23:13:13 +0530

    Add new file

---
+++ passgen.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+up_letters = string.ascii_uppercase
+low_letters = string.ascii_lowercase
+numbers = string.digits
+symbols = "!@#$%^&*()_}{|<>?"
+
+pool = up_letters + low_letters + numbers + symbols
+
+n = input("Enter number of chars: ")
+
+def gen(n):
+    print "".join(random.sample(pool, n))
+
+gen(n) 
