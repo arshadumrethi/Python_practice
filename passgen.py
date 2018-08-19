263e612472b4189b8ac16e7cf7863145b5acdca9
arshadumrethi <arshad.umrethi@gmail.com>
2018-08-19 18:38:33 +0530

    Add comments to files

--- passgen.py
+++ passgen.py
@@ -1,16 +1,23 @@
 import random
 import string
 
+
+#Setting Variables to store alphabets, numbers and symbols.
 up_letters = string.ascii_uppercase
 low_letters = string.ascii_lowercase
 numbers = string.digits
 symbols = "!@#$%^&*()_}{|<>?"
 
+
+#Setting variable to hold all characters in a pool.
 pool = up_letters + low_letters + numbers + symbols
 
+#Ask user how many characters to use, set it to a variable.
 n = input("Enter number of chars: ")
 
+#Create function that generates password using using random.
 def gen(n):
-    print "".join(random.sample(pool, n))
+    print "".join(random.sample(pool, n)) #random.sample takes a second argument specifies how many items to sample.
 
+#Call the function.
 gen(n)
